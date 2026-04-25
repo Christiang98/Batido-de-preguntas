72746b4b9502c4fec20ae07d28b2f9b0923fcf2d
Christiang98 <chrisu9855@gmail.com>
2026-04-24 23:45:41 -0300

    Tercera  version realizada

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Fuerza output estático puro — sin SSR, sin pre-render del servidor
-  // Ideal para apps Firebase que corren 100% en el cliente
-  output: 'export',
-}
+const nextConfig = {}
 module.exports = nextConfig
